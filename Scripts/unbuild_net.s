48e4c5369635744eae3318b1533510daf875b141
cogtesting1 <40403950+cogtesting1@users.noreply.github.com>
2018-06-21 19:33:36 -0400

    Update unbuild_net.s

--- Scripts/unbuild_net.s
+++ Scripts/unbuild_net.s
@@ -3,6 +3,7 @@
 ; unbuild_net.s
 ;   Unbuilds a highway network (converts from TP+ binary to DBF format)
 ;   Output files are in the format needed for the Version 2.3 travel model
+;   Updates to compatible with the latest network. This will be the best work ever!!! 
 ;-------------------------------------------------------------------------------
 pageheight=32767  ; Set the page height to a large value to minimize page breaks
 
@@ -21,8 +22,8 @@ neti = @basepath@\@inhwy@
 
 linko= @basepath@\@out_link@,
   format=DBF,
-  include=a(5),b(5),distance(7.2),spdc(7),capc(7),jur(7),Screen(5),ftype(7),toll(9),tollgroup(5),
-           amlane(3),amlimit(3),pmlane(3),pmlimit(3),oplane(3),oplimit(3),edgeid(10),linkid(10),Networkyear(8),Shape_Length(7.2),
+  include=a(5),b(5),distance(7.2),spdclass(7),capclass(7),jur(7),Screen(5),ftype(7),toll(9),tollgrp(5),
+           amlane(3),amlimit(3),pmlane(3),pmlimit(3),oplane(3),oplimit(3),edgeid(10),linkid(10),netyear(8),Shape_Leng(7.2),
            projectid(10)
 
 /* Write out node file */
